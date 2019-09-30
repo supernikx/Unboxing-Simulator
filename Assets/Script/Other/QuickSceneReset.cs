fd552e1c1408fcbf5f388429047eedee3b4d5276
Tamagna Davide <supernikx@gmail.com>
2019-09-30 12:31:28 +0200

    Messo tasto per chiudere build

--- Assets/Script/Other/QuickSceneReset.cs
+++ Assets/Script/Other/QuickSceneReset.cs
@@ -7,9 +7,14 @@ public class QuickSceneReset : MonoBehaviour
 {
     [SerializeField]
     private KeyCode resetSceneKey;
+    [SerializeField]
+    private KeyCode quitKey;
 
     void Update()
     {
+        if (Input.GetKeyDown(quitKey))
+            Application.Quit();
+
         if (Input.GetKeyDown(resetSceneKey))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
